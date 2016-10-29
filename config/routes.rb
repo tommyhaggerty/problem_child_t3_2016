8631a2cdf56191d5c7ed50a838ce1188e93feb17
Tommy Haggerty <tommy.haggerty@gmail.com>
2016-10-28 21:48:18 -0600

    Updated Problem_Child
    * Fixed Typos
    * Set a before action in the controller to 
      to define a robot by id paramater
    * Fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :rubots
+  resources :robots
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'robots#index'
 end
